a024a4c9697e2b1ad69f805d229d1afb13869a1a
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2020-12-11 16:00:24 +0100

    Refactored day11

--- src/aoc_2020_clojure/day11.clj
+++ src/aoc_2020_clojure/day11.clj
@@ -1,6 +1,4 @@
 (ns aoc-2020-clojure.day11
-  (:require [clojure.set :as set])
-  (:require [clojure.java.io :as io])
   (:require [clojure.string :as str]))
 
 (defn to-index [[x y] w]
@@ -9,85 +7,61 @@
 (defn from-index [i w]
   [(mod i w) (int (/ i w))])
 
-(defn iteration-p1 [index seat width height seats]
-  (let [
-        [sx sy] (from-index index width)
-        adj-x (map #(+ sx %) [-1 0 1])
-        adj-y (map #(+ sy %) [-1 0 1])
-        nx (filter #(and (>= % 0) (< % width)) adj-x)
-        ny (filter #(and (>= % 0) (< % height)) adj-y)
-        ns (filter #(not= % [sx sy]) (for [x nx y ny] [x y]))
-        ni (map #(to-index % width) ns)
-        neighbors (map #(seats %) ni)
-        occupied (count (filter #(= % "#") neighbors))]
-    (case seat
-      "." "."
-      "L" (if (= 0 occupied) "#" "L")
-      "#" (if (>= occupied 4) "L" "#"))
-    )
-  )
-
-(defn solve-p1 [data width height]
-  (loop [x data]
-    (let [new-data (vec (map-indexed #(iteration-p1 %1 %2 width height x) x))]
-
-      (if (= x new-data) x (recur new-data)))
-
-    )
-  )
-
-; part 1
-(time (as-> (slurp "data/input_d11") x
-            (str/split-lines x)
-            (map #(str/split % #"") x)
-            {:data (vec (flatten x)) :width (count (first x)) :height (count x)}
-            (solve-p1 (:data x) (:width x) (:height x))
-            (count (filter #(= % "#") x))
-            ))
-
-; part 2
-
-(defn find-visible-seat [[x y] [dx dy] data w h]
-  (loop [[px py] [x y]]
-    (let [np [(+ px dx) (+ py dy)]
-          [nx ny] np]
-      (if (or (< nx 0) (< ny 0) (>= nx w) (>= ny h))
-        nil
-        (if (= "L" (data (to-index np w)))
-          np
-          (recur np))))))
-
 (defn dirs []
   (filter #(not= % [0 0])
           (for [x [-1 0 1] y [-1 0 1]] [x y])))
 
-(defn visible-seats [index data w h]
-  (let [[x y] (from-index index w)
-        ds (dirs)]
-    (filter #(not= nil %) (map #(find-visible-seat [x y] % data w h) ds))))
+(defn seat-occupied? [seat] (= "#" seat))
+(defn seat-empty? [seat] (= "L" seat))
 
+(defn find-visible-seat
+  ([[x y] [dx dy] data w h] (find-visible-seat [x y] [dx dy] data w h 1))
+  ([[x y] [dx dy] data w h maxJumps]
+   (loop [[px py] [x y]
+          i 1]
+     (let [np [(+ px dx) (+ py dy)]
+           [nx ny] np]
+       (if (or (< nx 0) (< ny 0) (>= nx w) (>= ny h))
+         nil
+         (if (seat-empty? (data (to-index np w)))
+           np
+           (if (= i maxJumps) nil (recur np (inc i)))))))))
 
-(defn iteration-p2 [index seat w h data visible-seats]
-  (let [occupied (count (filter #(= "#" %) (map #(data (to-index % w)) visible-seats)))]
+(defn visible-seats
+  ([index data w h] (visible-seats index data w h 1))
+  ([index data w h maxJumps]
+   (let [[x y] (from-index index w)
+         ds (dirs)]
+     (filter #(not= nil %) (map #(find-visible-seat [x y] % data w h maxJumps) ds)))))
+
+(defn iteration [seat width data visible-seats occupiedLimit]
+  (let [occupied (count (filter seat-occupied? (map #(data (to-index % width)) visible-seats)))]
     (case seat
-      "." "."
       "L" (if (= 0 occupied) "#" "L")
-      "#" (if (>= occupied 5) "L" "#"))))
+      "#" (if (>= occupied occupiedLimit) "L" "#")
+      seat)))
 
-(defn solve-p2 [data width height visible-seats]
-  (loop [x data i 0]
-    (println i)
-    (let [new-data (vec (map-indexed #(iteration-p2 %1 %2 width height x (visible-seats %1)) x))]
-      (if (= x new-data) x (recur new-data (inc i))))))
+(defn solve [data width visible-seats occupiedLimit]
+  (count (filter seat-occupied?
+                 (loop [x data i 0]
+                   (let [new-data (vec (map-indexed #(iteration %2 width x (visible-seats %1) occupiedLimit) x))]
+                     (if (= x new-data) x (recur new-data (inc i))))))))
 
+(defn parse-data [input]
+  (as-> input x
+        (str/split-lines x)
+        (map #(str/split % #"") x)
+        {:data (vec (flatten x)) :width (count (first x)) :height (count x)}))
 
-; part 2
+(defn cache-visible-seats [x maxJumps]
+  (into {} (map-indexed (fn [i v] [i (visible-seats i (:data x) (:width x) (:height x) maxJumps)]) (:data x))))
+
+(def parts {:part1 {:occupiedLimit 4 :maxJumps 1} :part2 {:occupiedLimit 5 :maxJumps -1}})
+
+(defn prepare-data [x maxJumps]
+  (assoc x :visible-seats (cache-visible-seats x maxJumps)))
+
 (time (as-> (slurp "data/input_d11") x
-            (str/split-lines x)
-            (map #(str/split % #"") x)
-            {:data (vec (flatten x)) :width (count (first x)) :height (count x)}
-            (assoc x :visible-seats
-                     (into {} (map-indexed (fn [i v] [i (visible-seats i (:data x) (:width x) (:height x))]) (:data x))))
-            (solve-p2 (:data x) (:width x) (:height x) (:visible-seats x))
-            (count (filter #(= % "#") x))))
-
+            (parse-data x)
+            (map #(list (first %) (as-> (prepare-data x (:maxJumps (second %))) x
+                                        (solve (:data x) (:width x) (:visible-seats x) (:occupiedLimit (second %))))) parts)))
