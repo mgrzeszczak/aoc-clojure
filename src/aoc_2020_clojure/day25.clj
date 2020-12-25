e112e0a4a4d7ecf23e791edcbe578d15f0a667c5
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2020-12-25 06:12:06 +0100

    Added day 25

--- src/aoc_2020_clojure/day25.clj
+++ src/aoc_2020_clojure/day25.clj
@@ -2,12 +2,23 @@
   (:require [clojure.string :as str]
             [clojure.set :as set]))
 
-(defn parse-line [line]
-  line)
-
 (defn parse-input [file-name]
   (as-> (slurp file-name) x
         (str/split-lines x)
-        (map parse-line x)))
+        (map #(Long/parseLong %) x)))
 
-(time (as-> (parse-input "data/input_d25") x))
+(defn crack [v]
+  (loop [i 0
+         current 1]
+    (if (= current v)
+      i
+      (recur (inc i) (mod (* 7 current) 20201227)))))
+
+(defn transform [subject loop-size]
+  (reduce #(mod (* %1 %2) 20201227) 1 (repeat loop-size subject)))
+
+(defn solve [[v1 v2]]
+  (transform v2 (crack v1)))
+
+(time (as-> (parse-input "data/input_d25") x
+            (solve x)))
