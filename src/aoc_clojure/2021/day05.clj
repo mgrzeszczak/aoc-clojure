30b57544b128f27a501cb56c2b70f8fdcc53c2ac
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2021-12-05 10:16:41 +0100

    Refined day 5

--- src/aoc_clojure/2021/day05.clj
+++ src/aoc_clojure/2021/day05.clj
@@ -26,18 +26,9 @@
       (map + p1 (map * dir [n n])))))
 
 (defn update-state [state p1 p2]
-  (reduce #(assoc %1 %2 (inc (or (%1 %2) 0))) state (determine-path p1 p2))
-  )
+  (reduce #(assoc %1 %2 (inc (or (%1 %2) 0))) state (determine-path p1 p2)))
 
-(defn solve-p1 [lines]
-  (loop [[c & rest] (filter #(or (= (nth % 0) (nth % 2)) (= (nth % 1) (nth % 3))) lines)
-         state {}]
-    (if c
-      (let [[x1 y1 x2 y2] c]
-        (recur rest (update-state state [x1 y1] [x2 y2])))
-      (count (filter #(>= (second %) 2) state)))))
-
-(defn solve-p2 [lines]
+(defn solve [lines]
   (loop [[c & rest] lines
          state {}]
     (if c
@@ -46,9 +37,7 @@
       (count (filter #(>= (second %) 2) state)))))
 
 (time (as-> (parse-input "data/2021/d5") x
-            (solve-p1 x)
-            x))
+            (solve (filter #(or (= (nth % 0) (nth % 2)) (= (nth % 1) (nth % 3))) x))))
 
 (time (as-> (parse-input "data/2021/d5") x
-            (solve-p2 x)
-            x))
+            (solve x)))
