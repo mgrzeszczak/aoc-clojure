6274776abaf864e956b64d3986ef91c41c05a77c
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2021-12-06 23:17:29 +0100

    Reworked day 6.

--- src/aoc_clojure/2021/day06.clj
+++ src/aoc_clojure/2021/day06.clj
@@ -6,23 +6,18 @@
   (map #(Long/parseLong %) (str/split line #",")))
 
 (defn parse-input [file-name]
-  (as-> (slurp file-name) x
-        (str/split-lines x)
-        (map parse-line x)
-        (first x)))
+  (->> (slurp file-name)
+        (str/split-lines)
+        (map parse-line)
+        (first)))
 
-(defn solve [nums days]
-  (loop [i 0
-         state (into {} (map #(vec (list (first %) (count (second %)))) (group-by identity nums)))]
-    (if (= i days)
-      (reduce + (map second state))
-      (recur
-        (inc i)
-        (let [zeros (or (state 0) 0)
-              partial-state (map #(vec (list (dec (first %)) (second %))) (filter #(not= 0 (first %)) state))
-              with-new (into {} (if (> zeros 0) (cons [8 zeros] partial-state) partial-state))
-              six-count (or (with-new 6) 0)]
-          (assoc with-new 6 (+ six-count zeros)))))))
+(defn next[[zeros ones twos threes fours fives sixes sevens eights]]
+  [ones twos threes fours fives sixes (+ sevens zeros) eights zeros])
+
+(defn solve [xs days]
+  (let [data (group-by identity xs)
+        state (map #(or (count (data %)) 0) (range 9))]
+    (reduce + (reduce (fn [s _] (next s)) state (range days)))))
 
 (time (as-> (parse-input "data/2021/d6") x
             [(solve x 80) (solve x 256)]))
